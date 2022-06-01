f09b3a0420efb1cf38fefea56b417df45fed6ccb
Clair-Hawkes <hawkesj7@gmail.com>
2022-06-01 11:47:47 -0700

    Finish getShowsByTerm

--- tvmaze.js
+++ tvmaze.js
@@ -12,27 +12,71 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm( /* term */) {
+async function getShowsByTerm(searchTerm) {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
+  const allShows = [];
+  const tvMazeURL = 'https://api.tvmaze.com/search/shows';
 
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary 
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their 
-           normal lives, modestly setting aside the part they played in 
-           producing crucial intelligence, which helped the Allies to victory 
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She 
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+
+  const showInfo = await axios.get(tvMazeURL,{params: {q:searchTerm}});
+  const firstShow = {
+    id:showInfo.data[0].show.id,
+    name:showInfo.data[0].show.name,
+    summary:showInfo.data[0].show.summary,
+    image:showInfo.data[0].show.image.original
+  }
+  // for(let i=0;i<10;i++){
+  //   allShows.push({
+  //     id:showInfo.data[i].show.id,
+  //     name:showInfo.data[i].show.name,
+  //     summary:showInfo.data[i].show.summary,
+  //     image:showInfo.data[i].show.image.original
+  //   });
+  // }
+
+
+  for(let i=0;i<10;i++){
+
+    try{
+      allShows.push({
+        id:showInfo.data[i].show.id,
+        name:showInfo.data[i].show.name,
+        summary:showInfo.data[i].show.summary,
+        image:showInfo.data[i].show.image.original
+      });
+    } catch(err){
+      allShows.push({
+        id:showInfo.data[i].show.id,
+        name:showInfo.data[i].show.name,
+        summary:showInfo.data[i].show.summary,
+      });
     }
-  ]
+  }
+
+
+  console.log(allShows);
+  return allShows;
+
+
+  //PLACHOLDER
+  // return [
+  //   {
+  //     id: 1767,
+  //     name: "The Bletchley Circle",
+  //     summary:
+  //       `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
+  //          women with extraordinary skills that helped to end World War II.</p>
+  //        <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
+  //          normal lives, modestly setting aside the part they played in
+  //          producing crucial intelligence, which helped the Allies to victory
+  //          and shortened the war. When Susan discovers a hidden code behind an
+  //          unsolved murder she is met by skepticism from the police. She
+  //          quickly realises she can only begin to crack the murders and bring
+  //          the culprit to justice with her former friends.</p>`,
+  //     image:
+  //         "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+  //   }
+  // ]
 }
 
 
@@ -45,9 +89,9 @@ function populateShows(shows) {
     const $show = $(
         `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
-           <img 
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg" 
-              alt="Bletchly Circle San Francisco" 
+           <img
+              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
+              alt="Bletchly Circle San Francisco"
               class="w-25 me-3">
            <div class="media-body">
              <h5 class="text-primary">${show.name}</h5>
@@ -56,7 +100,7 @@ function populateShows(shows) {
                Episodes
              </button>
            </div>
-         </div>  
+         </div>
        </div>
       `);
 
