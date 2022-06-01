144ba67390e55f09207eb6a3a3f1ce901211e527
robin <robinnsheen@berkeley.edu>
2022-06-01 12:04:34 -0700

    click search to populate

--- tvmaze.js
+++ tvmaze.js
@@ -107,30 +107,6 @@ function populateShows(shows) {
     $showsList.append($show);  }
 }
 
-// function populateShows(shows) {
-//   $showsList.empty();
-
-//   for (let show of shows) {
-//     const $show = $(
-//         `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
-//          <div class="media">
-//            <img
-//               src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
-//               alt="Bletchly Circle San Francisco"
-//               class="w-25 me-3">
-//            <div class="media-body">
-//              <h5 class="text-primary">${show.name}</h5>
-//              <div><small>${show.summary}</small></div>
-//              <button class="btn btn-outline-light btn-sm Show-getEpisodes">
-//                Episodes
-//              </button>
-//            </div>
-//          </div>
-//        </div>
-//       `);
-
-//     $showsList.append($show);  }
-// }
 
 
 /** Handle search form submission: get shows from API and display.
